b3dd90f4792cafca639e0ecfd9df9e12d01266c5
Tongphasook Chayanin (2021) MKAI067 <chayanin.tongphasook.2021@live.rhul.ac.uk>
2023-05-27 21:09:07 +0100

    Product card

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -4,11 +4,41 @@ import styles from "./MainPage.scss";
 
 import Header from "../Header/Header";
 import Slider from "./Slider";
+import ProductCard from "./ProductCard";
+
+const imgRoot = process.env.PUBLIC_URL + "assets/img/";
+const productCards = [
+  {
+    image:
+      imgRoot +
+      "3.2.2 New Products (Add to existing list)/bio-island-dha-kids@2x.jpg",
+    title: "Bio Island DHA for Kids 100mg 60",
+    price: "AU$7.50 - $10.95",
+  },
+  {
+    image: imgRoot + "3.0 Main Page/blackmores-fish-oil@2x.png",
+    title: "Blackmores Cold Relief Bio-C 1000mg 180",
+    price: "AU$12.95 - $14.95",
+  },
+  {
+    image:
+      imgRoot +
+      "3.2.2 New Products (Add to existing list)/bio-island-dha-kids@2x.jpg",
+    title: "Nature’s Way Odourless Fish Oil 2000mg 200",
+    price: "AU$11.00 - $13.75",
+  },
+  {
+    image: imgRoot + "3.0 Main Page/blackmores-fish-oil@2x.png",
+    title: "Bio Island DHA for Kids 100mg 60",
+    price: "AU$7.50 - $10.95",
+  },
+];
 
 const MainPage = (props) => (
-  <div>
+  <div className="mainpage">
     <Header />
     <Slider />
+    <ProductCard product={productCards} />
   </div>
 );
 
---
+++ src/components/MainPage/ProductCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Slider from "react-slick";
+import styles from "./MainPage.scss";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProductCard = ({ product }) => {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Number of product cards to show at a time
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="product-card">
+      <h2>我的足迹</h2>
+      <Slider {...settings}>
+        {product.map((card) => (
+          <div key={card.id} className="product-card-slick">
+            <img src={card.image} alt={card.title} />
+            <h3>{card.title}</h3>
+            <p>{card.price}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ProductCard;
